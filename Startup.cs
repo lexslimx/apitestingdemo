6fc664ef42268bbda29c8ba1dc487cab6fb8f7a4
alex <alex.km@live.com>
2021-09-08 16:11:35 +0300

    enable dev settings

--- Startup.cs
+++ Startup.cs
@@ -36,13 +36,10 @@ namespace ApiTestingDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiTestingDemo v1"));
-            }
-
+     
             app.UseRouting();
 
             app.UseAuthorization();
