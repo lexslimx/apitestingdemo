fa74fd77ce2a17b0798bf12dbd55f9bb87dfab8b
alex <alex.km@live.com>
2021-09-08 17:23:40 +0300

    add error to weather api

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace ApiTestingDemo.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            throw new Exception("No");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
